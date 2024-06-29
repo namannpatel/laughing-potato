fe6131147d5b26384eb2eaaabd2ff40324a942f7
Naman Patel <62150420+namannpatel@users.noreply.github.com>
2024-06-29 17:35:24 +0000

    personal website update

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Naman Patel",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
+  email: "namanpatel024@gmail.com",
+  gitHub: "namannpatel",
+  instagram: "namannpatel",
   linkedIn: "satyanadella",
   medium: "",
   twitter: "microsoft",
